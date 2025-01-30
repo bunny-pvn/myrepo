74ccfebbe1d66fb5d676063d4d747bfa2dd9e1ed
bunny-pvn <bhagyasriananthapalli.2004@gmail.com>
2025-01-30 14:11:02 +0530

    Delete ArithmeticOperations directory

--- ArithmeticOperations/ArithmeticOperations/Model/Operand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ArithmeticOperations.Model
-{
-    public class Operand
-    {
-        public double FirstOperand { get; set; }
-        public double SecondOperand { get; set; }
-        public double Result { get; set; }
-    }
-}
